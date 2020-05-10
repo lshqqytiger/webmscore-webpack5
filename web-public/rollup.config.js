1bc610c02ff1181b8f204f74171573180f2b5558
Xmader <xmader@outlook.com>
2020-05-10 09:12:05 -0400

    chore: build mjs (ES Module) bundle

--- web-public/rollup.config.js
+++ web-public/rollup.config.js
@@ -1,21 +1,31 @@
 
-export default {
-    input: "src/index.js",
-    output: {
-        file: "webmscore.js",
-        format: "iife",
-        name: 'WebMscore',
-        exports: 'default',
-        sourcemap: false,
-    },
-    plugins: [
-        {
-            resolveImportMeta(property) {
-                if (property === 'url') {
-                    return '""';
-                }
-                return null;
-            },
+export default [
+    {
+        input: "src/index.js",
+        output: {
+            file: "webmscore.js",
+            format: "iife",
+            name: 'WebMscore',
+            exports: 'default',
+            sourcemap: false,
         },
-    ],
-}
+        plugins: [
+            {
+                resolveImportMeta(property) {
+                    if (property === 'url') {
+                        return '""';
+                    }
+                    return null;
+                },
+            },
+        ],
+    },
+    {
+        input: "src/index.js",
+        output: {
+            file: "webmscore.mjs",
+            format: "esm",
+            sourcemap: false,
+        }
+    }
+]
