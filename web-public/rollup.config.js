c0734b038a2ee05f48ffcaf4ce65e30e1e4e7ac9
Xmader <xmader@outlook.com>
2020-12-18 03:23:13 -0500

    feat: support legacy browsers that have no WebAssembly support

--- web-public/rollup.config.js
+++ web-public/rollup.config.js
@@ -4,14 +4,23 @@ import babel from '@rollup/plugin-babel'
 const WEBPACK_IMPORT = `
 import libWasm from '!!file-loader?name=[name].wasm!./webmscore.lib.wasm-'  // workaround for Webpack 4
 import libData from '!!file-loader?name=[name].[ext]!./webmscore.lib.data'
+
+// legacy browser support
+import libWasmJs from '!!file-loader?name=[name].[ext]!./webmscore.lib.wasm.js'
+import libMem from '!!file-loader?name=[name].[ext]!./webmscore.lib.js.mem'
 `
 
 const WEBPACK_LOCATE_FILE = `
+// modern browsers that support WebAssembly 
 if (path.endsWith('.wasm')) return new URL(MSCORE_LIB_WASM, MSCORE_BASEURL).href
 if (path.endsWith('.data')) return new URL(MSCORE_LIB_DATA, MSCORE_BASEURL).href
+
+// legacy browser support
+if (path.endsWith('.wasm.js')) return new URL(MSCORE_LIB_WASM_JS, MSCORE_BASEURL).href
+if (path.endsWith('.js.mem')) return new URL(MSCORE_LIB_MEM, MSCORE_BASEURL).href
 `
 
-const WEBPACK_WORKER_IMPORT = '+ `var MSCORE_LIB_WASM = "${libWasm}", MSCORE_LIB_DATA = "${libData}", MSCORE_BASEURL = "${document.baseURI}";`'
+const WEBPACK_WORKER_IMPORT = '+ `var MSCORE_LIB_WASM = "${libWasm}", MSCORE_LIB_DATA = "${libData}", MSCORE_LIB_WASM_JS = "${libWasmJs}", MSCORE_LIB_MEM = "${libMem}", MSCORE_BASEURL = "${document.baseURI}";`'
 
 const INJECTION_HINT = (n) => `// %INJECTION_HINT_${n}%`
 
@@ -28,6 +37,15 @@ const INJECT_WEBPACK_LOCATE_FILE = {
     transform(code) {
         code = code.replace(INJECTION_HINT(0), WEBPACK_LOCATE_FILE)
         code = code.replace(INJECTION_HINT(1), WEBPACK_WORKER_IMPORT)
+        return { code }
+    }
+}
+
+const BYPASS_EVAL_WARNING = {
+    transform(code, id) {
+        if (id.includes(".lib.js")) {
+            code = code.replace(/eval\(/g, "var eval_=eval;eval_(")
+        }
         return { code }
     }
 }
@@ -57,7 +75,7 @@ export default [
             banner: "export const WebMscoreWorker = function () { ",
             footer: "}\n",
         },
-        plugins: [REPLACE_IMPORT_META],
+        plugins: [REPLACE_IMPORT_META, BYPASS_EVAL_WARNING],
     },
     {
         input: "src/worker-helper.js",
@@ -85,7 +103,7 @@ export default [
             exports: "default",
             sourcemap: false,
         },
-        plugins: [REPLACE_IMPORT_META],
+        plugins: [REPLACE_IMPORT_META, BYPASS_EVAL_WARNING],
     },
     {
         input: "src/worker-helper.js",
