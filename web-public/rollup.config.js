6a69ab9c57bf8ff7f8b62d2ed2c9fb5e267e3342
Xmader <xmader@outlook.com>
2021-01-12 14:00:21 -0500

    feat: serve assets by cdn

--- web-public/rollup.config.js
+++ web-public/rollup.config.js
@@ -1,10 +1,21 @@
 
+import fs from 'fs'
 import babel from '@rollup/plugin-babel'
+import { version } from './package.json'
 
 const WEBPACK_IMPORT = `
 import libWasm from '!!file-loader?name=[name].wasm!./webmscore.lib.wasm-'  // workaround for Webpack 4
 import libData from '!!file-loader?name=[name].[ext].wasm!./webmscore.lib.data'
 import libMem from '!!file-loader?name=webmscore.lib.mem.wasm!./webmscore.lib.js.mem'
+`
+
+const CDN_IMPORT = `
+const CDN_PROVIDER = 'https://cdn.jsdelivr.net/npm'
+const URL_PREFIX = CDN_PROVIDER + '/webmscore@%VERSION%/' // https://cdn.jsdelivr.net/npm/webmscore@ver
+
+const libWasm = URL_PREFIX + 'webmscore.lib.wasm'
+const libData = URL_PREFIX + 'webmscore.lib.data'
+const libMem = URL_PREFIX + 'webmscore.lib.js.mem'
 `
 
 const WEBPACK_LOCATE_FILE = `
@@ -60,6 +71,25 @@ const BABEL_PLUGIN = babel({
     exclude: ['.cache/**'],
 })
 
+const INJECT_REGENERATOR_RUNTIME = {
+    transform(code) {
+        if (code.includes("regeneratorRuntime")) {
+            const runtimeModule = fs.readFileSync('node_modules/regenerator-runtime/runtime.js', 'utf-8')
+            // use L9-L726 only
+            const runtimeFn = runtimeModule.split('\n').slice(8, 725).join("\n")
+            code = `var regeneratorRuntime = (function (exports) {\n${runtimeFn}\n})({})\n` + code
+        }
+        return { code }
+    }
+}
+
+const WEBPACK_TRANSFORM_PLUGINS = [
+    REPLACE_IMPORT_META,
+    INJECT_WEBPACK_LOCATE_FILE,
+    BABEL_PLUGIN,
+    INJECT_REGENERATOR_RUNTIME,
+]
+
 export default [
     {
         input: "src/worker.js",
@@ -108,10 +138,16 @@ export default [
             banner: WEBPACK_IMPORT,
             sourcemap: false,
         },
-        plugins: [
-            REPLACE_IMPORT_META,
-            INJECT_WEBPACK_LOCATE_FILE,
-            BABEL_PLUGIN,
-        ],
+        plugins: WEBPACK_TRANSFORM_PLUGINS,
+    },
+    {
+        input: "src/worker-helper.js",
+        output: {
+            file: "webmscore.cdn.mjs",
+            format: "esm",
+            banner: CDN_IMPORT.replace("%VERSION%", version),
+            sourcemap: false,
+        },
+        plugins: WEBPACK_TRANSFORM_PLUGINS,
     }
 ]
